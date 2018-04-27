5d4b2c95ae74799eb3438618d9717d058362e52f
Mochammad Dicky Adriansyah <v.mochammad.adriansyah@adira.co.id>
2018-04-27 11:21:48 +0700

    perubahan data

--- WebApi/Services/Container/CompleteDataSiswa.cs
+++ WebApi/Services/Container/CompleteDataSiswa.cs
@@ -1,27 +1,45 @@
 ﻿using Entites.Data;
+using Entites.Master;
 using Entites.Request;
 using InterfaceApi.DataAccess;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utilities;
 
 namespace Services.Container
 {
     internal class CompleteDataSiswa
     {
+        internal CompleteDataSiswa()
+        {
+            siswa = new siswa();
+        }
 
         internal CompleteDataSiswa(GenericRequest request)
         {
             this.request = request;
-            siswa = new siswa();
+            siswa_data = JsonConvert.DeserializeObject<SiswaEntity>(request.json_data);
+            siswa_data.EmptyStringtoNull();
+            siswa = new siswa
+            {
+                id_siswa = siswa_data.id_siswa,
+                nama_siswa = siswa_data.nama_siswa,
+                alamat = siswa_data.alamat
+            };
         }
 
-        internal GenericRequest request;
+        internal SiswaEntity siswa_data { get; set; }
 
-        internal siswa siswa;
-
+        //internal DataSiswa data_siswa { get; set; }
+        
         internal IUnitOfWork repository { get; set; }
+
+        internal GenericRequest request { get; set; }
+
+        internal siswa siswa { get; set; }
     }
 }
