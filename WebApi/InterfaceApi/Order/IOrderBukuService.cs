556f8dd0160e93faabae26480a5bab91a530547f
Mochammad Dicky Adriansyah <v.mochammad.adriansyah@adira.co.id>
2018-05-04 15:18:39 +0700

    Interface Order Buku

---
+++ WebApi/InterfaceApi/Order/IOrderBukuService.cs
@@ -0,0 +1,14 @@
+﻿using Entites.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceApi.Order
+{
+    public interface IOrderBukuService
+    {
+        GenericResponse OrderBukuProses(GenericRequest @params);
+    }
+}
