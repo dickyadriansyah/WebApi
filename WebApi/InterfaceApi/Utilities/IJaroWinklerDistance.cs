7dcdda8d79cb56d7739fc7f89f268065fa4ef182
Mochammad Dicky Adriansyah <v.mochammad.adriansyah@adira.co.id>
2018-05-04 15:39:47 +0700

    Penambahan Algoritma

---
+++ WebApi/InterfaceApi/Utilities/IJaroWinklerDistance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceApi.Utilities
+{
+    public interface IJaroWinklerDistance
+    {
+        double proximity(string aString1, string aString2);
+    }
+}
