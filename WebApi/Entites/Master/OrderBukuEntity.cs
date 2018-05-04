c9463c84d93b64d381d1830ae03a9a39ee4ce8f4
Mochammad Dicky Adriansyah <v.mochammad.adriansyah@adira.co.id>
2018-05-04 15:15:08 +0700

    Penambahan Orderbuku entites

---
+++ WebApi/Entites/Master/OrderBukuEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entites.Master
+{
+    public class OrderBukuEntity
+    {
+        public string id_siswa { get; set; }
+
+        public List<string> list_nama_buku { get; set; }
+    }
+}
