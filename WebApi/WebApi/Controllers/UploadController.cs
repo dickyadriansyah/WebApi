a9e08c5d5866a33644f275442467e7e5f8a044f4
Mochammad Dicky Adriansyah <v.mochammad.adriansyah@adira.co.id>
2018-05-11 11:33:22 +0700

    Contoller API Upload

---
+++ WebApi/WebApi/Controllers/UploadController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class UploadController : ApiController
+    {
+
+        [Route("v1/UploadImage")]
+        [AllowAnonymous]
+        public async Task<HttpResponseMessage> UploadImage()
+        {
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            try
+            {
+                var httpRequest = HttpContext.Current.Request;
+
+                foreach (string file in httpRequest.Files)
+                {
+                    HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created);
+                    var postedFile = httpRequest.Files[file];
+                    if(postedFile !=null && postedFile.ContentLength > 0)
+                    {
+                        int MaxContentLength = 1024 * 1024 * 1;
+
+                        IList<string> AllowedFileExtensions = new List<string> { ".jpg", ".gif", ".png" };
+                        var ext = postedFile.FileName.Substring(postedFile.FileName.LastIndexOf('.'));
+                        var extension = ext.ToLower();
+                        if (!AllowedFileExtensions.Contains(extension))
+                        {
+                            var message = string.Format("Tolong Upload Image dengan type .jpg, .gif, .png");
+
+                            dict.Add("error", message);
+                            return Request.CreateResponse(HttpStatusCode.BadRequest, dict);
+                        }
+                        else if(postedFile.ContentLength > MaxContentLength)
+                        {
+                            var message = string.Format("Tolong Upload file dengan ukuran minimal 1 mb");
+                            dict.Add("error", message);
+                            return Request.CreateResponse(HttpStatusCode.BadRequest, dict);
+                        }
+                        else
+                        {
+                            var filePath = HttpContext.Current.Server.MapPath("~/UserImage/" + postedFile.FileName + extension);
+                            postedFile.SaveAs(filePath);
+                        }
+                    }
+                    var message1 = string.Format("Image Updated Successfully");
+                    return Request.CreateErrorResponse(HttpStatusCode.Created, message1);
+                }
+                var res = string.Format("Please Upload a image");
+                dict.Add("error", res);
+                return Request.CreateResponse(HttpStatusCode.NotFound, dict);
+            }catch(Exception ex)
+            {
+                var res = string.Format("some Message");
+                dict.Add("error", res);
+                return Request.CreateResponse(HttpStatusCode.NotFound, dict);
+            }
+        }
+
+    }
+}
