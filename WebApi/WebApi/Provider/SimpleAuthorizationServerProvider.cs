95e6a1d3c849d1229b79d9761c78760a89b3a440
Mochammad Dicky Adriansyah <v.mochammad.adriansyah@adira.co.id>
2018-05-11 17:29:50 +0700

    Oauth Token

---
+++ WebApi/WebApi/Provider/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebApi.Provider
+{
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+
+            context.Validated(new ClaimsIdentity(context.Options.AuthenticationType));
+        }
+    }
+}
